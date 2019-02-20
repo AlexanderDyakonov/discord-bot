e5243a10d58f17fbb52fdf81834690df39739e66
AlexanderDyakonov <31617303+AlexanderDyakonov@users.noreply.github.com>
2019-02-20 20:04:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const client = new Discord.Client();
+const config = require("./config.json");
+const randomPuppy = require('random-puppy');
+const cp = require('child_process');
+const path = require('path');
+
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+
+/*client.on("ready", () => {
+  console.log("I am ready!");
+});*/
+
+fs.readdir(__dirname + "/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir(__dirname + "/commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token);
